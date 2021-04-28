79c63d9483c8c300f499fbb0a2a04fd8d8b6731b
nonepostlife <ilyha575@inbox.ru>
2021-04-28 12:48:14 +1000

    add task for homework 1

---
+++ src/main/java/ru/postlife/java/homework1/HomeWorkApp.java
@@ -0,0 +1,44 @@
+package ru.postlife.java.homework1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    static private void printThreeWords()
+    {
+        System.out.println("_Orange\n"
+                +"_Banana\n"
+                +"_Apple");
+    }
+
+    static private void checkSumSign()
+    {
+        int a = 5;
+        int b = -18;
+        String answer = (a + b) > 0 ? "Сумма положительная" : "Сумма отрицательная";
+        System.out.println(answer);
+    }
+    static private void printColor()
+    {
+        int value = 123;
+        if(value <= 0)
+            System.out.println("Красный");
+        else if (value <= 100)
+            System.out.println("Желтый");
+        else
+            System.out.println("Зеленый");
+    }
+
+    static private void compareNumbers()
+    {
+        int a = 5;
+        int b = 18;
+        String answer = (a >= b) ? "a >= b" : "a < b";
+        System.out.println(answer);
+    }
+}
