62e848b93b757c55d03c1bf26418ffca295c41f4
Ilya <ilyha575@inbox.ru>
2021-09-07 16:18:51 +1000

    added lesson 6

---
+++ src/main/java/ru/postlife/ads/lesson6/Test.java
@@ -0,0 +1,45 @@
+package ru.postlife.ads.lesson6;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Tree<Integer> tree = new TreeImpl<>();
+
+        tree.add(60);
+        tree.add(50);
+//        tree.add(66);
+        tree.add(40);
+        tree.add(55);
+        tree.add(70);
+        tree.add(31);
+        tree.add(45);
+        tree.add(42);
+        tree.add(43);
+        tree.add(69);
+        tree.add(67);
+        tree.add(68);
+        tree.add(81);
+
+        System.out.println("Find 70: " + tree.contains(70));
+        System.out.println("Find 700: " + tree.contains(700));
+
+        tree.traverse(Tree.TraversMode.IN_ORDER);
+        tree.traverse(Tree.TraversMode.PRE_ORDER);
+        tree.traverse(Tree.TraversMode.POST_ORDER);
+
+/*        tree.display();
+
+//        tree.remove(43);
+//        tree.remove(67);
+//        tree.remove(45);
+//        tree.remove(66);
+//
+//        tree.remove(40);
+        tree.remove(60);
+
+        tree.display();*/
+
+
+
+    }
+}
---
+++ src/main/java/ru/postlife/ads/lesson6/TreeImpl.java
@@ -0,0 +1,259 @@
+package ru.postlife.ads.lesson6;
+
+import java.util.Stack;
+
+public class TreeImpl<E extends Comparable<? super E>> implements Tree<E> {
+
+    private Node<E> root;
+    private int size;
+
+
+    private class NodeAndParent {
+        private Node<E> current;
+        private Node<E> parent;
+
+        public NodeAndParent(Node<E> current, Node<E> parent) {
+            this.current = current;
+            this.parent = parent;
+        }
+
+    }
+
+    @Override
+    public boolean contains(E value) {
+        NodeAndParent nodeAndParent = doFind(value);
+        return nodeAndParent.current != null;
+    }
+
+    private NodeAndParent doFind(E value) {
+        Node<E> current = root;
+        Node<E> parent = null;
+
+        while (current != null) {
+            if (current.getValue().equals(value)) {
+                return new NodeAndParent(current, parent);
+            }
+
+            parent = current;
+            if (current.isLeftChild(value)) {
+                current = current.getLeftChild();
+            } else {
+                current = current.getRightChild();
+            }
+            //l++
+        }
+        return new NodeAndParent(null, parent);
+    }
+
+    @Override
+    public boolean add(E value) {
+
+        Node<E> node = new Node<>(value);
+
+        NodeAndParent nodeAndParent = doFind(value);
+
+        if (nodeAndParent.current != null) {
+            return false;
+        }
+
+        //if (nAP.level > maxLevels) return false;
+        Node<E> parent = nodeAndParent.parent;
+
+        if (parent == null) {
+            root = node;
+        } else if (parent.isLeftChild(value)) {
+            parent.setLeftChild(node);
+        } else {
+            parent.setRightChild(node);
+        }
+        size++;
+
+        return true;
+    }
+
+    @Override
+    public boolean remove(E value) {
+        NodeAndParent nodeAndParent = doFind(value);
+        Node<E> removedNode = nodeAndParent.current;
+        Node<E> parentNode = nodeAndParent.parent;
+
+        if (removedNode == null) {
+            return false;
+        }
+
+        if (removedNode.isList()) {
+            removeLeafNode(removedNode, parentNode);
+        } else if (removedNode.hasOnlyOneChild()) {
+            removeNodeWithOneChild(removedNode, parentNode);
+        } else {
+            removeNodeWithAllChildren(removedNode, parentNode);
+        }
+        size--;
+        return true;
+    }
+
+    private void removeNodeWithAllChildren(Node<E> removedNode, Node<E> parentNode) {
+        Node<E> successor = getSuccessor(removedNode);
+
+        if (removedNode == root) {
+            root = successor;
+        } else if (parentNode.isLeftChild(removedNode.getValue())) {
+            parentNode.setLeftChild(successor);
+        } else {
+            parentNode.setRightChild(successor);
+        }
+        successor.setLeftChild(removedNode.getLeftChild());
+
+    }
+
+    private Node<E> getSuccessor(Node<E> removedNode) {
+        Node<E> successor = removedNode;
+        Node<E> successorParent = null;
+        Node<E> current = removedNode.getRightChild();
+
+        while (current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.getLeftChild();
+        }
+
+        if (successor != removedNode.getRightChild() && successorParent != null) {
+            successorParent.setLeftChild(successor.getRightChild());
+            successor.setRightChild(removedNode.getRightChild());
+        }
+
+        return successor;
+    }
+
+    private void removeNodeWithOneChild(Node<E> removedNode, Node<E> parentNode) {
+        Node<E> childNode = removedNode.getLeftChild() != null
+                ? removedNode.getLeftChild()
+                : removedNode.getRightChild();
+
+        if (removedNode == root) {
+            root = childNode;
+        } else if (parentNode.isLeftChild(removedNode.getValue())) {
+            parentNode.setLeftChild(childNode);
+        } else {
+            parentNode.setRightChild(childNode);
+        }
+    }
+
+    private void removeLeafNode(Node<E> removedNode, Node<E> parentNode) {
+        if (removedNode == root) {
+            root = null;
+        } else if (parentNode.isLeftChild(removedNode.getValue())) {
+            parentNode.setLeftChild(null);
+        } else {
+            parentNode.setRightChild(null);
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void display() {
+        Stack<Node<E>> globalStack = new Stack<>();
+        globalStack.push(root);
+        int nBlanks = 64;
+
+        boolean isRowEmpty = false;
+        System.out.println("................................................................");
+
+        while (!isRowEmpty) {
+            Stack<Node<E>> localStack = new Stack<>();
+
+            isRowEmpty = true;
+            for (int i = 0; i < nBlanks; i++) {
+                System.out.print(" ");
+            }
+
+            while (!globalStack.isEmpty()) {
+                Node<E> tempNode = globalStack.pop();
+                if (tempNode != null) {
+                    System.out.print(tempNode.getValue());
+                    localStack.push(tempNode.getLeftChild());
+                    localStack.push(tempNode.getRightChild());
+                    if (tempNode.getLeftChild() != null || tempNode.getRightChild() != null) {
+                        isRowEmpty = false;
+                    }
+                } else {
+                    System.out.print("--");
+                    localStack.push(null);
+                    localStack.push(null);
+                }
+                for (int j = 0; j < nBlanks * 2 - 2; j++) {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+
+            while (!localStack.isEmpty()) {
+                globalStack.push(localStack.pop());
+            }
+
+            nBlanks /= 2;
+        }
+        System.out.println("................................................................");
+    }
+
+    @Override
+    public void traverse(TraversMode mode) {
+        switch (mode) {
+            case PRE_ORDER: {
+                preOrder(root);
+                break;
+            }
+            case IN_ORDER: {
+                inOrder(root);
+                break;
+            }
+            case POST_ORDER: {
+                postOrder(root);
+                break;
+            }
+            default:
+                throw new RuntimeException("Unknown travers mode: " + mode);
+        }
+        System.out.println();
+    }
+
+    private void postOrder(Node<E> current) { // обратный порядок
+        if (current == null) {
+            return;
+        }
+
+        postOrder(current.getLeftChild());
+        postOrder(current.getRightChild());
+        System.out.print(current.getValue() + " ");
+    }
+
+    private void inOrder(Node<E> current) { //центрированный обход
+        if (current == null) {
+            return;
+        }
+
+        inOrder(current.getLeftChild());
+        System.out.print(current.getValue() + " ");
+        inOrder(current.getRightChild());
+    }
+
+    private void preOrder(Node<E> current) { //прямой обход
+        if (current == null) {
+            return;
+        }
+
+        System.out.print(current.getValue() + " ");
+        preOrder(current.getLeftChild());
+        preOrder(current.getRightChild());
+    }
+}
